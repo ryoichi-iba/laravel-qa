0fe459b7a1ddad76494695750456249804ffedcb
Ryoichi Iba <ryoichiaz18@gmail.com>
2020-02-17 18:42:08 +0700

    Prepuplate users

--- app/Question.php
+++ app/Question.php
@@ -14,5 +14,11 @@ class Question extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function setTitleAttribute($value){
+
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+
+    }
     
 }
